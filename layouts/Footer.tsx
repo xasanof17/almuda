8bd77932f186a40e6b42927a24714ebbcaf26601
Javohir <xasanof17@gmail.com>
2023-08-30 20:06:58 +0500

    updated meta opengraphs

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -48,18 +48,16 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   const currentDate = new Date().getHours();
 
-  console.log(currentDate);
-
   return (
-    <footer className="container">
-      <div className="grid grid-cols-4 gap-5">
+    <footer className="shadow">
+      <div className="container grid grid-cols-4 gap-5">
         <div className="flex flex-col items-start">
           <Link href="/" title="Al-Muda Logo">
             <div className="relative h-12 w-[150px]">
               <Image src={Logo} alt="Al-Muda logo" fill />
             </div>
           </Link>
-          <ul className="mt-4 flex items-center space-x-2">
+          <ul className="mt-4 flex items-center space-x-3">
             {socials.map(({ Icon, name, href }, i) => (
               <li key={i}>
                 <Link
@@ -68,14 +66,14 @@ const Footer = () => {
                   href={href}
                   className="flex items-center justify-center"
                 >
-                  <Icon className="muted h-7 w-7 transition-colors duration-300 hover:text-blue-500" />
+                  <Icon className="muted h-6 w-6 transition-colors duration-300 hover:text-bright md:h-7 md:w-7" />
                 </Link>
               </li>
             ))}
           </ul>
         </div>
       </div>
-      <div className="mt-3 flex w-full items-center justify-center space-x-1 border-t-2 py-5">
+      <div className="mt-8 flex w-full items-center justify-center space-x-1 border-t-2 py-5">
         <p className="muted text-base font-medium md:text-lg">
           &copy; All rights reserved.
         </p>
