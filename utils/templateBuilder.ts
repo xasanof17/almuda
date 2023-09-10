3bb7209d73ff69cdc3399f0a243bdd00cd3647ae
Javohir <xasanof17@gmail.com>
2023-09-10 18:27:39 +0500

    update template builder

--- utils/templateBuilder.ts
+++ utils/templateBuilder.ts
@@ -2,24 +2,26 @@ export function templateBuilder<T extends {}>(data: any): T {
   if (Object.keys(data).includes("companyName")) {
     return {
       ...data,
+      from_name: data["email"],
       message: `
-        Hi my name is ${data["firstName"]} ${data["lastName"]}.\n
-        My Company name is ${data["companyName"]}.\n
+        Hi my name is ${data["firstName"]} ${data["lastName"]}.
+        My Company name is ${data["companyName"]}.
         My contacts: ${data["phoneNumber"]}.
-        Message: ${data["message"]}
+        Message: ${data["message"]}.
       `,
     };
   } else {
     return {
       ...data,
+      from_name: data["email"],
       message: `
         My business activity: ${data["business_activity"]["label"]},
         I will require type of: ${data["premises"]["label"]},
         Owners: ${data["owners"]},
         Vizas: ${data["vizas"]},
-        My name is ${data["lastName"]} ${data["firstName"]},\n
+        My name is ${data["lastName"]} ${data["firstName"]},
         My phone number is: ${data["phoneNumber"]},
-        My email is: ${data["email"]}
+        My email is: ${data["email"]}.
       `,
     };
   }
