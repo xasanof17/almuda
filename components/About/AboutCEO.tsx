dd005688b85f53857dd109dedc41ceedff244a5e
Javohir <xasanof17@gmail.com>
2023-09-22 14:56:11 +0500

    fix bug

--- components/About/AboutCEO.tsx
+++ components/About/AboutCEO.tsx
@@ -13,7 +13,7 @@ export const AboutCEO = () => {
             className="rounded-3xl bg-top object-contain sm:object-cover lg:object-contain"
           />
         </div>
-        <div className="relative block h-[400px] w-[400px] md:hidden">
+        <div className="relative block h-[400px] w-full md:hidden md:w-[400px]">
           <Image
             src={CEO}
             alt="CEO of Almuda"
@@ -24,14 +24,8 @@ export const AboutCEO = () => {
         </div>
       </div>
       <div className="flex flex-col justify-center lg:col-span-2">
-        <Image
-          src={Quote}
-          alt="Quote Icon"
-          width={24}
-          height={24}
-          priority
-        />
-        <p className="text-muted xs:text-[17px] my-4 w-full text-left text-base sm:text-lg sm:leading-6 sm:tracking-wide md:leading-8">
+        <Image src={Quote} alt="Quote Icon" width={24} height={24} priority />
+        <p className="text-muted xs:text-[17px] xs:text-left my-4 w-full text-center text-base sm:text-lg sm:leading-6 sm:tracking-wide md:leading-8">
           The healthcare industry has never been so advanced, and yet there are
           still so many patients who don&apos;t receive the right care at the
           right time. Infermedica thrives in the face of this global challenge
@@ -41,13 +35,13 @@ export const AboutCEO = () => {
         </p>
         <h3
           title="Oisha Rakhimova"
-          className="mx-4 text-xl font-bold tracking-wide text-secondary sm:text-2xl sm:leading-9 md:mx-0"
+          className="text-xl font-bold tracking-wide text-secondary sm:text-2xl sm:leading-9 md:mx-0"
         >
           Oisha Rakhimova
         </h3>
         <p
           title="Almuda CEO"
-          className="text-muted  mx-4 text-left text-[17px] leading-6 tracking-wide sm:text-lg sm:leading-8 md:mx-0"
+          className="text-muted text-left text-[17px] leading-6 tracking-wide sm:text-lg sm:leading-8 md:mx-0"
         >
           Almuda CEO
         </p>
