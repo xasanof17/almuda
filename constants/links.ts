9b2639cceba78c150b42a5220dc75a333fe6b1bd
Javohir <xasanof17@gmail.com>
2023-09-25 17:07:29 +0500

    links

--- constants/links.ts
+++ constants/links.ts
@@ -7,14 +7,7 @@ export const links = [
     title: "About",
     href: "/about",
   },
-  {
-    title: "Careers",
-    href: "/careers",
-  },
-  {
-    title: "FAQ",
-    href: "/faq",
-  },
+
   {
     title: "Contact",
     href: "/contact",
