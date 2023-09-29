57a454f54fc775a1de075544dacce057d4f92a16
Javohir <xasanof17@gmail.com>
2023-09-29 15:56:54 +0500

    robots

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${baseUrl }/sitemap.xml`,
+  };
+}
