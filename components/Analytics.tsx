33c67d9b870790256e4da5b875f6d0fbadeeca01
Javohir <xasanof17@gmail.com>
2023-10-09 17:05:53 +0300

    update keywords

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -7,11 +7,16 @@ declare global {
 
 export default function Analytics() {
   const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   if (!token) {
     return null;
   }
   return (
     <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+      />
       <script
         src="https://beamanalytics.b-cdn.net/beam.min.js"
         data-token={token}
