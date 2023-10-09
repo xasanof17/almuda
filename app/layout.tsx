e7a503416ee44e13d29220d4f6af584430a84485
Xasanov Javohir <93818267+xasanof17@users.noreply.github.com>
2023-10-09 15:04:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,13 +72,13 @@ export const metadata: Metadata = {
     ],
   },
   robots: {
-    index: false,
+    index: true,
     follow: true,
-    nocache: true,
+    // nocache: true,
     googleBot: {
       index: true,
-      follow: false,
-      noimageindex: true,
+      follow: true,
+      noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
